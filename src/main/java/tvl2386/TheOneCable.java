e72a18cd8b5ba022ead981ed86c2630683f8f950
Tom van Leeuwen <tom.van.leeuwen@artofautomation.net>
2017-12-05 17:10:45 +0100

    first commit

---
+++ src/main/java/tvl2386/TheOneCable.java
@@ -0,0 +1,20 @@
+package tvl2386;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = TheOneCable.MODID, version = TheOneCable.VERSION)
+public class TheOneCable
+{
+    public static final String MODID = "theonecable";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    }
+}
